b53835a3e283448e93c8177cb9e5aa97866427a3
Edward Faulkner <ef@alum.mit.edu>
2014-11-13 17:30:34 -0500

    initial work

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-'use strict';
+var StubGenerator = require('./stub-generator');
+var browserify = require('broccoli-browserify');
 
-module.exports = {
-  name: 'ember-browserify'
+// 'modules' is an output -- it will gather up the list of modules
+// that we packaged, so you can whitelist them during es6-compilation.
+module.exports = function emberBrowserify(tree, modules){
+  return browserify(StubGenerator(tree, modules), {
+      entries: ['./browserify_stubs.js']
+  });
 };
---
+++ stub-generator.js
@@ -0,0 +1,47 @@
+var walkSync = require('walk-sync');
+var Petal = require('petal');
+var fs = require('fs');
+var writeFile = require('broccoli-file-creator');
+
+module.exports = StubGenerator;
+
+function StubGenerator(inputTree, modules) {
+  if (!(this instanceof StubGenerator)) {
+    return new StubGenerator(inputTree, modules);
+  }
+  this.inputTree = inputTree;
+  this.modules = modules;
+  this.stubs = {};
+}
+
+StubGenerator.prototype.cleanup = function(){};
+
+StubGenerator.prototype.read = function(readTree) {
+  var self = this;
+  return readTree(this.inputTree).then(function(srcDir){
+    var paths = walkSync(srcDir);
+    paths.forEach(function (relativePath) {
+      if (relativePath.slice(-3) === '.js') {
+        var petal = new Petal(relativePath, fs.readFileSync(srcDir + '/' + relativePath));
+        Object.keys(petal.imports).forEach(function(key){
+          if (key.slice(0,4) === 'npm:') {
+            var moduleName = key.slice(4);
+            self.modules[moduleName] = [ 'default' ];
+            self.stubs[moduleName] = true;
+          }
+        });
+      }
+    });
+    return writeFile('browserify_stubs.js', self.generate()).read(readTree);
+  });
+};
+
+StubGenerator.prototype.generate = function() {
+  return Object.keys(this.stubs).map(function(moduleName){
+    return "define('npm:" +
+      moduleName +
+      "', function(){ return require('" +
+      moduleName +
+      "');})";
+  }).join("\n");
+};
