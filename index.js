774da74f9a75cc7549b1504be403f74e4b7b32fb
Edward Faulkner <ef@alum.mit.edu>
2014-11-14 14:55:45 -0500

    Updating from just a broccoli filter to an addon

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var StubGenerator = require('./stub-generator');
 var browserify = require('broccoli-browserify');
+var mergeTrees = require('broccoli-merge-trees');
 
-module.exports = function emberBrowserify(tree){
-  return browserify(StubGenerator(tree), {
-      entries: ['./browserify_stubs.js']
-  });
+module.exports = {
+  name: 'ember-browserify',
+
+  included: function(app){
+    app.import('browserify/browserify.js');
+  },
+
+  postprocessTree: function(type, tree){
+    if (type === 'js'){
+      var bundle = browserify(StubGenerator(tree), {
+        entries: ['./browserify_stubs.js'],
+        outputFile: './browserify/browserify.js'
+      });
+      return mergeTrees([tree, bundle]);
+    } else
+      return tree;
+  }
 };
