da04fe2e02876eae528308b7730da2c1a79d69b1
Edward Faulkner <ef@alum.mit.edu>
2014-11-14 17:39:04 -0500

    don't preserve stubs across rebuilds

--- stub-generator.js
+++ stub-generator.js
@@ -10,7 +10,6 @@ function StubGenerator(inputTree) {
     return new StubGenerator(inputTree);
   }
   this.inputTree = inputTree;
-  this.stubs = {};
 }
 
 StubGenerator.prototype.cleanup = function(){};
@@ -19,23 +18,25 @@ StubGenerator.prototype.read = function(readTree) {
   var self = this;
   return readTree(this.inputTree).then(function(srcDir){
     var paths = walkSync(srcDir);
+    var stubs = {};
     paths.forEach(function (relativePath) {
       if (relativePath.slice(-3) === '.js') {
+
         var petal = new Petal(relativePath, fs.readFileSync(srcDir + '/' + relativePath));
         Object.keys(petal.imports).forEach(function(key){
           if (key.slice(0,4) === 'npm:') {
             var moduleName = key.slice(4);
-            self.stubs[moduleName] = true;
+            stubs[moduleName] = true;
           }
         });
       }
     });
-    return writeFile('browserify_stubs.js', self.generate()).read(readTree);
+    return writeFile('browserify_stubs.js', self.generate(stubs)).read(readTree);
   });
 };
 
-StubGenerator.prototype.generate = function() {
-  return Object.keys(this.stubs).map(function(moduleName){
+StubGenerator.prototype.generate = function(stubs) {
+  return Object.keys(stubs).map(function(moduleName){
     return "define('npm:" +
       moduleName +
       "', function(){ return { default: require('" +
