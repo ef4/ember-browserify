887a3a106f747440eaefecf3f3cece5358ea768a
Edward Faulkner <ef@alum.mit.edu>
2014-11-15 01:49:25 -0500

    simplify by inheriting from broccoli-caching-writer

--- stub-generator.js
+++ stub-generator.js
@@ -5,72 +5,52 @@ var helpers = require('broccoli-kitchen-sink-helpers');
 var RSVP = require('rsvp');
 var rimraf = RSVP.denodeify(require('rimraf'));
 var symlinkOrCopy = require('symlink-or-copy');
-var quickTemp = require('quick-temp');
-var path = require('path');
-var generateRandomString = require('./rand');
+var CachingWriter = require('broccoli-caching-writer');
 
-module.exports = StubGenerator;
+module.exports = CachingWriter.extend({
+  enforceSingleInputTree: true,
 
-// This module is inspired broccoli-caching-writer, but it has a more
-// tailored caching strategy.
+  init: function(){
 
-function StubGenerator(inputTree){
-  this.inputTree = inputTree;
+    // The petalCache lets us avoid re-parsing individual files that
+    // haven't changed.
+    this.petalCache = {};
 
-  // The petalCache lets us avoid re-parsing individual files that
-  // haven't changed.
-  this.petalCache = {};
+    // The stubsCache lets us avoid re-running browserify when the set
+    // of included modules hasn't changed.
+    this.stubsCache = {};
+  },
 
-  // The stubsCache lets us avoid re-running browserify when the set
-  // of included modules hasn't changed.
-  this.stubsCache = {};
-
-  this._destDir = path.resolve(path.join('tmp', 'stub-generator-dest-dir_' + generateRandomString(6) + '.tmp'));
-}
-
-StubGenerator.prototype.cleanup = function () {
-  quickTemp.remove(this, 'tmpCacheDir');
-  rimraf.sync(this._destDir);
-};
-
-StubGenerator.prototype.getCacheDir = function () {
-  return quickTemp.makeOrReuse(this, 'tmpCacheDir');
-};
-
-StubGenerator.prototype.getCleanCacheDir = function () {
-  return quickTemp.makeOrRemake(this, 'tmpCacheDir');
-};
-
-
-StubGenerator.prototype.read = function(readTree) {
-  var self = this;
-  return readTree(this.inputTree).then(function(srcDir){
-    var paths = walkSync(srcDir);
-    var stubs = {};
-    var updateCacheResult;
-    paths.forEach(function (relativePath) {
-      if (relativePath.slice(-3) === '.js') {
-        gatherStubs(srcDir, relativePath, stubs, self.petalCache);
+  read: function(readTree) {
+    var self = this;
+    return readTree(this.inputTrees[0]).then(function(srcDir){
+      var paths = walkSync(srcDir);
+      var stubs = {};
+      var updateCacheResult;
+      paths.forEach(function (relativePath) {
+        if (relativePath.slice(-3) === '.js') {
+          gatherStubs(srcDir, relativePath, stubs, self.petalCache);
+        }
+      });
+      if (!sameStubs(stubs, self.stubsCache)) {
+        self.stubsCache = stubs;
+        updateCacheResult = self.updateCache(srcDir, self.getCleanCacheDir());
       }
+      return updateCacheResult;
+    }).then(function(){
+      return rimraf(self._destDir);
+    }).then(function(){
+      symlinkOrCopy.sync(self.getCacheDir(), self._destDir);
+    }).then(function(){
+      return self._destDir;
     });
-    if (!sameStubs(stubs, self.stubsCache)) {
-      self.stubsCache = stubs;
-      updateCacheResult = self.updateCache(srcDir, self.getCleanCacheDir());
-    }
-    return updateCacheResult;
-  }).then(function(){
-    return rimraf(self._destDir);
-  }).then(function(){
-    symlinkOrCopy.sync(self.getCacheDir(), self._destDir);
-  }).then(function(){
-    return self._destDir;
-  });
-};
+  },
 
-StubGenerator.prototype.updateCache = function(srcDir, destDir) {
-  fs.writeFileSync(destDir + '/browserify_stubs.js', generate(this.stubsCache));
-  return destDir;
-};
+  updateCache: function(srcDir, destDir) {
+    fs.writeFileSync(destDir + '/browserify_stubs.js', generate(this.stubsCache));
+    return destDir;
+  }
+});
 
 
 function gatherStubs(srcDir, relativePath, stubs, cache) {
