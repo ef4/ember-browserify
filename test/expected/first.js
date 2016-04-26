53ccc75f3a26f35056e190f799293b2ab333b2eb
Stefan Penner <stefan.penner@gmail.com>
2016-04-26 13:24:49 -0700

    refactor stub-generator
    * [PERF] only readFiles/process files if they changed
    * [REFACTOR] extract stub generation code
    * [PERF] only update output if changed

--- test/expected/first.js
+++ test/expected/first.js
@@ -1,3 +1,3 @@
+define('npm:broccoli', function(){ return { 'default': require('broccoli')};})
 define('npm:x', function(){ return { 'default': require('x')};})
 define('npm:y', function(){ return { 'default': require('y')};})
-define('npm:broccoli', function(){ return { 'default': require('broccoli')};})
