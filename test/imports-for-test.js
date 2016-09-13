d9bf432cb7af0c3bc17e6afc76d55f6914130c0e
Stefan Penner <stefan.penner@gmail.com>
2016-09-13 16:12:38 -0700

    more ember-cli 1.x related removals

--- test/imports-for-test.js
+++ test/imports-for-test.js
@@ -2,13 +2,6 @@ var expect = require('chai').expect;
 var importsFor = require('../lib/imports-for');
 
 describe('importsFor', function() {
-  it('parses ES6', function() {
-    expect(importsFor('import asdf from "npm:asdf"')).to.eql({ asdf: true });
-    expect(importsFor('import asdf from "npm:asdf"')).to.eql({ asdf: true });
-    expect(importsFor('import asdf from "npm:asdf"')).to.eql({ asdf: true });
-    expect(importsFor('import apple from "npm:foo"')).to.eql({ foo:  true });
-  })
-
   it('parses AMD (ES5)', function() {
     expect(importsFor('define("asdf", ["npm:asdf"], function() { });')).to.eql({ asdf: true });
     expect(importsFor('define("asdf", ["npm:asdf"], function() { });')).to.eql({ asdf: true });
