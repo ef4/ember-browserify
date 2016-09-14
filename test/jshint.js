cdf0067a292d1853505261f23ff3b1f97fade038
Stefan Penner <stefan.penner@gmail.com>
2016-09-14 12:35:09 -0700

    add jshinting to tests

--- test/jshint.js
+++ test/jshint.js
@@ -1,3 +1,9 @@
-require('mocha-jshint')([
-  'lib'
-]);
+var walkSync = require('walk-sync');
+var path = require('path');
+
+require('mocha-jshint')({
+  paths: ['lib'].concat(walkSync(__dirname, { ignore: ['fixtures'] }).
+    map(function(relativePath) {
+      return path.join(__dirname, relativePath);
+    }))
+});
