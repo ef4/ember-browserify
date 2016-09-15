a7f4b5da3eda37a9e81fe9ca0cca35b9dbbd0cac
Nathan Hammond <github.com@nathanhammond.com>
2016-09-15 15:40:56 -0700

    Set up top-level addon fixtures.

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -3,8 +3,34 @@ import Ember from 'ember';
 // Should be version 1970.0.0
 import flooring from 'npm:flooring';
 
+// Should be 1970.0.0, which the host app includes
+import modernApp from '../utils/floor-modern';
+import outdatedApp from '../utils/floor-outdated';
+
+// Should be version 1970.0.0
+import outdated from 'outdated/utils/floor-type';
+
+// Should be version 2010.0.0
+import modern from 'modern/utils/floor-type';
+
 export default Ember.Route.extend({
   model() {
     return flooring();
+  },
+
+  modern() {
+    return modern();
+  },
+
+  modernApp() {
+    return modernApp();
+  },
+
+  outdated() {
+    return outdated();
+  },
+
+  outdatedApp() {
+    return outdatedApp();
   }
 });
