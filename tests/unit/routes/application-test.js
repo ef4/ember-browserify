61df56d06b0d048a1fa243df76620b0a6f455861
Nathan Hammond <github.com@nathanhammond.com>
2016-09-15 15:40:56 -0700

    Set up top-level addon fixtures.

--- tests/unit/routes/application-test.js
+++ tests/unit/routes/application-test.js
@@ -7,5 +7,16 @@ moduleFor('route:application', 'Unit | Route | application', {
 
 test('it properly resolves npm modules', function(assert) {
   let route = this.subject();
+
+  // Host application which depends on flooring@1970
+  // Set by virtue of folder structure.
   assert.equal(route.model(), 'shag carpet');
+  assert.equal(route.modernApp(), 'shag carpet');
+  assert.equal(route.outdatedApp(), 'shag carpet');
+
+  // Addon which depends on flooring@2010
+  assert.equal(route.modern(), 'hardwood');
+
+  // Addon which depends on flooring@1970
+  assert.equal(route.outdated(), 'shag carpet');
 });
