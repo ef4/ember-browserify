2c686a02e491007a067916f7c7f153e135020cae
Stefan Penner <stefan.penner@gmail.com>
2016-09-16 14:56:39 -0700

    update symlink/junction hack to work in more cases

--- bin/install-ember-addons.js
+++ bin/install-ember-addons.js
@@ -6,13 +6,12 @@ var rimraf = require('rimraf');
 var fs = require('fs');
 
 ['modern', 'outdated'].forEach(function(inRepoAddon) {
+  var source = path.resolve(__dirname, '..', 'lib/');
+  var target = path.resolve(__dirname, '..', 'tests/dummy/lib', inRepoAddon, 'node_modules/ember-browserify/lib/');
 
-  var source = path.resolve(__dirname, '..', 'lib/index.js');
-  var directory = path.resolve(__dirname, '..', 'tests/dummy/lib', inRepoAddon, 'node_modules/ember-browserify/lib/');
-  var target = path.join(directory, 'index.js');
   console.log('rimraf', target);
-
   rimraf.sync(target);
+
   console.log('symlink', source, target)
   symlinkOrCopy.sync(source, target);
 });
