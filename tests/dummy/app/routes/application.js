03c9c9577e64742e9ffbbe9c22c13971f6e58a60
Stefan Penner <stefan.penner@gmail.com>
2016-09-20 08:41:08 -0700

    add comments, making it easier to understand where stuff is actually coming from.

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -4,16 +4,16 @@ import Ember from 'ember';
 import flooring from 'npm:flooring';
 
 // Should be 2030.0.0, which the host app includes
-import modernApp from '../utils/floor-modern';
-import outdatedApp from '../utils/floor-outdated';
+import modernApp from '../utils/floor-modern'; // source: tests/dummy/lib/modern/app/utils/floor-modern
+import outdatedApp from '../utils/floor-outdated'; // source: tests/dummy/lib/outdated/app/utils/floor-outdated
 
 // Should be version 1970.0.0
 import outdated from 'outdated/utils/floor-type';
-import outdatedReexports from '../utils/reexports-outdated';
+import outdatedReexports from '../utils/reexports-outdated'; // source: tests/dummy/lib/outdated/app/utils/reexports-outdated
 
 // Should be version 2010.0.0
 import modern from 'modern/utils/floor-type';
-import modernReexports from '../utils/reexports-modern';
+import modernReexports from '../utils/reexports-modern'; // source: tests/dummy/lib/modern/app/utils/reexports-modern
 
 export default Ember.Route.extend({
   flooring,
